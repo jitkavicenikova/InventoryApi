909f749fa03159817cfd40804e380f26e7cdcc22
jitkavicenikova <493352@mail.muni.cz>
2025-10-15 19:54:12 +0200

    setup database

---
+++ InventoryApi/Data/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using InventoryApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApi.Data;
+
+public class InventoryDbContext : DbContext
+{
+    public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
+        : base(options) { }
+
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<Stock> Stocks { get; set; } = null!;
+    public DbSet<StockMovement> StockMovements { get; set; } = null!;
+}
--- InventoryApi/Program.cs
+++ InventoryApi/Program.cs
@@ -1,3 +1,6 @@
+using InventoryApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,9 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<InventoryDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<InventoryDbContext>();
+    db.Database.Migrate();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {

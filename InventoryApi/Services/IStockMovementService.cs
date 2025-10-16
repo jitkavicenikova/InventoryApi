34eacc3ffad8027f0429cc2cad718d3fe9135e13
jitkavicenikova <493352@mail.muni.cz>
2025-10-16 21:00:47 +0200

    implement stock movement service

--- InventoryApi/Services/IStockMovementService.cs
+++ InventoryApi/Services/IStockMovementService.cs
@@ -5,5 +5,5 @@ namespace InventoryApi.Services;
 
 public interface IStockMovementService
 {
-    Task<StockMovementDto> CreateAsync(int stockId, int quantityChange, MovementType type);
+    Task CreateAsync(int stockId, int quantityChange, MovementType type);
 }
---
+++ InventoryApi/Services/StockMovementService.cs
@@ -0,0 +1,53 @@
+﻿using InventoryApi.Data;
+using InventoryApi.Entities;
+using InventoryApi.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApi.Services;
+
+public class StockMovementService : IStockMovementService
+{
+    private readonly InventoryDbContext _context;
+
+    public StockMovementService(InventoryDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateAsync(int stockId, int quantityChange, MovementType type)
+    {
+        var stock = await _context.Stocks
+            .Include(s => s.StockMovements)
+            .FirstOrDefaultAsync(s => s.Id == stockId)
+            ?? throw new KeyNotFoundException($"Stock with ID {stockId} not found.");
+
+        UpdateStockQuantity(stock, quantityChange, type);
+
+        var movement = new StockMovement
+        {
+            StockId = stockId,
+            Stock = stock,
+            QuantityChange = quantityChange,
+            MovementType = type,
+            Timestamp = DateTime.UtcNow
+        };
+
+        _context.StockMovements.Add(movement);
+        await _context.SaveChangesAsync();
+    }
+
+    private static void UpdateStockQuantity(Stock stock, int quantityChange, MovementType type)
+    {
+        if (type == MovementType.Incoming)
+        {
+            stock.Quantity += quantityChange;
+        }
+        else if (type == MovementType.Outgoing)
+        {
+            if (stock.Quantity < quantityChange)
+                throw new InvalidOperationException("Not enough items in stock for outgoing movement.");
+
+            stock.Quantity -= quantityChange;
+        }
+    }
+}

834c5401a4ea91ceeddd32463672db1a2f0a04ce
jitkavicenikova <493352@mail.muni.cz>
2025-10-17 16:53:29 +0200

    add isDeleted property to Stock entity

--- InventoryApi/Entities/Stock.cs
+++ InventoryApi/Entities/Stock.cs
@@ -7,6 +7,7 @@ public class Stock
     public int Id { get; set; }
     public int Quantity { get; set; }
     public Unit Unit { get; set; }
+    public bool IsDeleted { get; set; }
 
     public int ProductId { get; set; }
     public required Product Product { get; set; }
